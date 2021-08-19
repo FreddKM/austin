458c62984ca7f7f6bf9011ff1035f96fa19c6690
Freddy Mande <freddoss18@gmail.com>
2021-08-19 14:33:06 -0500

    add git ignore

--- cool.py
+++ cool.py
@@ -1,3 +1,33 @@
 # cool things
 # More cool stuff
 # even more
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
+# stufff
